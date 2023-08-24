276b3f26c6dbba24141813912a39baabb1e09700
meghanapatelgp <meghanapatelgp02@gmail.com>
2023-08-24 19:21:47 +0530

    updated

--- sporty_shoes_project/src/main/java/com/sporty_shoes_pages/AddToCart.java
+++ sporty_shoes_project/src/main/java/com/sporty_shoes_pages/AddToCart.java
@@ -2,10 +2,8 @@ package com.sporty_shoes_pages;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
 
 public class AddToCart {
 	private WebDriver driver;
